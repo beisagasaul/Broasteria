33459c9bbd1090099ac36a4a358ecf472d92e421
beisagasaul <beisagasaul21@gmail.com>
2024-04-25 09:05:12 -0700

    configuracion

--- backend_broasteria/src/main.ts
+++ backend_broasteria/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI,defaultVersion:'1'  });
+  app.enableCors();
+  await app.listen(process.env.PORT);
+  console.log(`App running on ${await app.getUrl()}`);
+  
 }
 bootstrap();
