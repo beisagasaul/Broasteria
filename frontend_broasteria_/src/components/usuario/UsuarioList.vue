a1f7f69c8bc0fa4160ee48873aa6575dd377cefc
RODRICL <carreonlambertinrodrigo@gmail.com>
2024-05-18 17:15:03 -0400

    creacion usuarios en fronted

---
+++ frontend_broasteria_/src/components/usuario/UsuarioList.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import type { Usuario } from '@/models/usuario'   //hace referencia al modelo cliente 
+import { onMounted, ref } from 'vue'  
+import http from '@/plugins/axios'
+import router from '@/router'
+
+const props = defineProps<{
+  ENDPOINT_API: string
+}>()
+
+const ENDPOINT = props.ENDPOINT_API ?? ''
+var usuarios = ref<Usuario[]>([])
+
+//obtener
+async function getUsuarios() {
+  clientes.value = await http.get(ENDPOINT).then((response) => response.data)
+}
+
+//editar
+function toEdit(id: number) {
+  router.push(`/clientes/editar/${id}`)
+}
+//elimnar
+async function toDelete(id: number) {
+  var r = confirm('¿Está seguro que se desea eliminar el Cliente?')
+  if (r == true) {
+    await http.delete(`${ENDPOINT}/${id}`).then(() => getclientes())
+  }
+}
+
+onMounted(() => {
+  getClientes()
+})
+</script>
+
+
+<template>
+  <div class="container">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><RouterLink to="/">Inicio</RouterLink></li>
+        <li class="breadcrumb-item active" aria-current="page">Clientes</li>
+      </ol>
+    </nav>
+
+    <div class="row">
+      <h2>Lista de Usuarios</h2>
+      <div class="col-12">
+        <RouterLink to="/usuarios/crear"
+        ><font-awesome-icon icon="fa-solid fa-plus" title="Crear Nuevo" /></RouterLink>
+      </div>
+    </div>
+
+    <div class="table-responsive">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">N°</th>
+            <th scope="col">Nombres</th>
+             <th scope="col">Email</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          <tr v-for="(usuario, index) in usuarios.values()" :key="usuario.id">
+            <th scope="row">{{ index + 1 }}</th>
+            <td>{{ usuario. nombreUsuario }}</td>
+            <td>{{ usuario. email }}</td>
+
+            <td>
+              <button class="btn btn-link" @click="toEdit(usuario.id)">
+              <font-awesome-icon icon="fa-solid fa-edit" title="Editar" />
+              </button>
+
+
+
+              <button class="btn btn-link" @click="toDelete(usuario.id)"
+              ><font-awesome-icon icon="fa-solid fa-trash" title="Elimnar" /></button>
+            </td>
+
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+
+<style scoped></style>
---
+++ frontend_broasteria_/src/models/usuario.ts
@@ -0,0 +1,7 @@
+export interface Cliente {
+  id: number
+
+  nombreUsuario: string
+
+  email: string
+}
