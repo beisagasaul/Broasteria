a8c3bf266e971d71619a034bb6b27e91efdcf987
beisagasaul <beisagasaul21@gmail.com>
2024-06-11 22:55:50 -0700

    crear arreglado de ventas

--- frontend_broasteria_/src/components/venta/VentaCreate.vue
+++ frontend_broasteria_/src/components/venta/VentaCreate.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue"; // Importa onMounted desde Vue
 import http from "@/plugins/axios";
 import router from "@/router";
-import type { Venta } from '@/models/venta'
-import type { Cliente } from '@/models/cliente'
+import type { Venta } from '@/models/venta';
+import type { Cliente } from '@/models/cliente';
 
 const props = defineProps<{
   ENDPOINT_API: string;
 }>();
 
 const ENDPOINT = props.ENDPOINT_API ?? "";
-const venta = ref<Venta[]>({} as Venta);
-const clientes = ref<Cliente[]>([])
+const venta = ref<Venta>({} as Venta); // Debe ser ref<Venta> en lugar de ref<Venta[]>
+const clientes = ref<Cliente[]>([]);
 
 async function crearVenta() {
   await http
@@ -22,14 +22,19 @@ async function crearVenta() {
     .then(() => router.push("/ventas"));
 }
 
-onMounted(async () => {
-  clientes.value = await http.get('clientes').then((res) => res.data)
-})
+async function getClientes() {
+  clientes.value = await http.get('clientes').then((res) => res.data);
+}
+
+onMounted(() => {
+  getClientes();
+});
 
 function goBack() {
   router.go(-1);
 }
 </script>
+
 
 <template>
   <div class="container">
@@ -49,21 +54,19 @@ function goBack() {
 
     <div class="row">
       <form @submit.prevent="crearVenta">
-        <div class="form-floating mb-3">
-          <select
-            class="form-select"
-            v-model="venta.cliente"
-            placeholder="TotalVenta"
-            required
-          />
-          <option value="":disabled="true">Seleccione un elemento</option>
+
+
+        <div class="form-floating mb-2">
+          <select class="form-select" v-model="venta.cliente" required>
+          <option value="" :disabled="true">Seleccione un elemento</option>
           <option v-for="cliente in clientes" :key="cliente.id" value="cliente">
             {{ cliente.nombres }}
-          </option>
-          <label for="totalVenta">Total de Venta</label>
+          </option> 
+          </select>
+          <label for="cliente">Cliente</label>
         </div>
 
-        <div class="form-floating mb-3">
+        <div class="form-floating mb-2">
           <input
             type="text"
             class="form-control"
@@ -73,6 +76,7 @@ function goBack() {
           />
           <label for="totalVenta">Total de Venta</label>
         </div>
+
 
         <div class="text-center mt-3">
           <button type="submit" class="btn btn-primary btn-lg">
